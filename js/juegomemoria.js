e4d6469303153c83057b05385fe9d3ea7181a9c9
Luciano <lucianocanton18@gmail.com>
2020-08-21 00:26:06 -0300

    randomized button numbers

--- js/juegomemoria.js
+++ js/juegomemoria.js
@@ -7,12 +7,23 @@ const green = document.getElementById('greenbutton')
 class Game{
   constructor(){
     this.begin()
+    this.randomNumber()
   }
   begin(){
     alert('GOOOOO')
+    this.nivel = 1
+    this.colors = {
+      red,
+      blue,
+      yellow,
+      green
+    }
+  }
+  randomNumber(){
+    this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
   }
 }
 
 function beginGame(){
-  var game = new Game()
+  window.game = new Game()
 }
