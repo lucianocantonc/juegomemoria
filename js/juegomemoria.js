538c779fb8fa48c33b1dbab1a3a158e2f91715a2
Luciano <lucianocanton18@gmail.com>
2020-08-21 01:40:00 -0300

    random light added to the buttons

--- js/juegomemoria.js
+++ js/juegomemoria.js
@@ -1,17 +1,21 @@
-const beginbtn = document.getElementById('beginbutton')
-const red = document.getElementById('redbutton')
-const blue = document.getElementById('bluebutton')
-const yellow = document.getElementById('yellowbutton')
-const green = document.getElementById('greenbutton')
+
+
+const beginbtn = document.getElementById("beginbutton")
+const red = document.getElementById("redbutton")
+const blue = document.getElementById("bluebutton")
+const yellow = document.getElementById("yellowbutton")
+const green = document.getElementById("greenbutton")
+
 
 class Game{
   constructor(){
     this.begin()
     this.randomNumber()
+    this.nextLevel()
   }
   begin(){
     alert('GOOOOO')
-    this.nivel = 1
+    this.level = 7
     this.colors = {
       red,
       blue,
@@ -19,8 +23,36 @@ class Game{
       green
     }
   }
+  numberToColor(number){
+    switch (number){
+      case 0:
+        return 'red'
+      case 1:
+        return 'blue'
+      case 2:
+        return 'yellow' 
+      case 3:
+        return 'green'
+    }
+  }
   randomNumber(){
     this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+  }
+  nextLevel(){
+    this.lightUpSequence()
+  }
+  lightUpSequence(){
+    for (let i = 0; i < this.level; i++){
+      const color = this.numberToColor(this.sequence[i])
+      setTimeout(() => this.lightUpColor(color), 1000 * i)
+    }
+  }
+  lightUpColor(color){
+    this.colors[color].classList.add('light')
+    setTimeout(() => this.shutDownColor(color), 400) 
+  }
+  shutDownColor(color){
+    this.colors[color].classList.remove('light')
   }
 }
 
